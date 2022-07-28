a94c1da6ba5d9c603c8e88491e481930fb70aec3
ArifinRafi <65380126+ArifinRafi@users.noreply.github.com>
2022-07-29 05:49:20 +0600

    Update hextrovert_control.ino

--- hextrovert_control.ino
+++ hextrovert_control.ino
@@ -1,31 +1,16 @@
-int R_IS = 14;
-int L_IS = 15;
-int R_EN = 16;
-int L_EN = 17;
+int R_IS = 6;
+int L_IS = 7;
+int R_EN = 8;
+int L_EN = 9;
 
-//motor1
+//motor1, motor3 and motor5
 int RPWM_1 = 2;
 int LPWM_1 = 3;
 
-//motor2
+//motor2, motor4 and motor 6
 int RPWM_2 = 4;
 int LPWM_2 = 5;
 
-//motor3
-int RPWM_3 = 6;
-int LPWM_3 = 7;
-
-//motor4
-int RPWM_4 = 8;
-int LPWM_4 = 9;
-
-//motor5
-int RPWM_5 = 10;
-int LPWM_5 = 11;
-
-//motor6
-int RPWM_6 = 12;
-int LPWM_6 = 13;
 
 int speed1 = 150;
 int speed2 = 100;
@@ -40,29 +25,13 @@ void setup() {
   pinMode(R_EN, OUTPUT);
   pinMode(L_EN, OUTPUT);
   
-  //Motor 1
+  //motor1, motor3 and motor5
   pinMode(RPWM_1 , OUTPUT);
   pinMode(LPWM_1, OUTPUT);
 
-  //Motor 2
+  //motor2, motor4 and motor6
   pinMode(RPWM_2 , OUTPUT);
   pinMode(LPWM_2, OUTPUT);
-
-  //Motor 3
-  pinMode(RPWM_3 , OUTPUT);
-  pinMode(LPWM_3, OUTPUT);
-
-  //Motor 4
-  pinMode(RPWM_4 , OUTPUT);
-  pinMode(LPWM_4, OUTPUT);
-
-  //Motor 5
-  pinMode(RPWM_5 , OUTPUT);
-  pinMode(LPWM_5, OUTPUT);
-
-  //Motor 6
-  pinMode(RPWM_6 , OUTPUT);
-  pinMode(LPWM_6, OUTPUT);
   
 
   digitalWrite(R_IS, LOW);
@@ -102,41 +71,24 @@ void loop(){
 void forward() {
   analogWrite(RPWM_1, speed1);
   analogWrite(RPWM_2, speed1);
-  analogWrite(RPWM_3, speed1);
-  analogWrite(RPWM_4, speed1);
-  analogWrite(RPWM_5, speed1);
-  analogWrite(RPWM_6, speed1);
 }
 
 void backward() {
   analogWrite(LPWM_1, speed1);
   analogWrite(LPWM_2, speed1);
-  analogWrite(LPWM_3, speed1);
-  analogWrite(LPWM_4, speed1);
-  analogWrite(LPWM_5, speed1);
-  analogWrite(LPWM_6, speed1);
 }
 
 
 void right() {
   analogWrite(RPWM_1, speed2);
-  analogWrite(RPWM_3, speed2);
-  analogWrite(RPWM_5, speed2);
-  
   analogWrite(LPWM_2, speed2);
-  analogWrite(LPWM_4, speed2);
-  analogWrite(LPWM_6, speed2);
+  
 }
 
 void left() {
   
   analogWrite(RPWM_2, speed2);
-  analogWrite(RPWM_4, speed2);
-  analogWrite(RPWM_6, speed2);
-
   analogWrite(LPWM_1, speed2);
-  analogWrite(LPWM_3, speed2);
-  analogWrite(LPWM_5, speed2);
 }
 
 void reset() {
